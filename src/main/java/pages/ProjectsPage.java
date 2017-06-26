05bea10693635aaca6414fcde23276b17a20aff3
osandadeshan <osanda.deshan@gmail.com>
2017-06-26 10:39:04 +0530

    Changed to use for every projects by capturing the project element from its name.

--- src/main/java/pages/ProjectsPage.java
+++ src/main/java/pages/ProjectsPage.java
@@ -1,7 +1,13 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import stepImpl.DriverFactory;
+
+import static stepImpl.DriverFactory.driver;
 
 /**
  * Created by Osanda on 6/21/2017.
@@ -9,6 +15,9 @@ import stepImpl.DriverFactory;
 
 
 public class ProjectsPage extends BasePage {
+
+    @FindBy(xpath = "//span[text()[contains(., 'Go to the Project')]]")
+    WebElement GO_TO_PROJECT_BUTTON;
 
     public static String ProjectsPageUrl = URL.concat("?path_info=projects");
     WebDriver driver = DriverFactory.driver;
@@ -20,7 +29,12 @@ public class ProjectsPage extends BasePage {
 
     public void goToProjectTasks(String projectName) throws InterruptedException {
         waitSeconds(2);
-        driver.get(ProjectsPageUrl + "%2F" + projectName + "%2Ftasks");
+        PageFactory.initElements(driver, this);
+        WebElement PROJECT_NAME = driver.findElement(By.xpath("//table/tbody/tr[td//text()[contains(., '" + projectName + "')]]"));
+        PROJECT_NAME.click();
+        waitForElementClickable(GO_TO_PROJECT_BUTTON);
+        GO_TO_PROJECT_BUTTON.click();
+        driver.get(driver.getCurrentUrl() + "%2Ftasks");
     }
 
 }
